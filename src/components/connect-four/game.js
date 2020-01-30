3d94be58b614c8cf4f7274aefd20e7f67fbf6516
Dave Trost <davetrost@gmail.com>
2020-01-30 14:33:52 -0800

    import connect4-ai into the game, report game result (win/draw)

--- src/components/connect-four/game.js
+++ src/components/connect-four/game.js
@@ -1,37 +1,45 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Board from '../board/Board';
+import Connect4 from 'connect4-ai';
+
+const game = new Connect4();
 
 const ConnectFour = ({ playerNames }) => {
   const colors = ['red', 'black'];
   const columns = 7;
   const rows = 6;
+  const activeGame = '';
   if(!playerNames) {
     playerNames = ['player 1', 'player 2'];
   }
+
+  const togglePlayer = () => playerIndex ? setPlayerIndex(0) : setPlayerIndex(1);
+  const getPlayerName = () => playerNames[playerIndex];
+  const getPlayerColor = () => colors[playerIndex];
+  const gameIsActive = () => gameResult === activeGame;
   
+  const makeId = (col, row) => `${col},${row}`;
+  const getRow = id => +id[2];
+  const getColumn = id => +id[0];
+  const getAboveId = id => makeId(getColumn(id), getRow(id) - 1);
+
   const initialBoard = Array.from({ length: rows * columns }, (_, i) => {
     const row = Math.floor(i / columns);
     const col = i % columns;
     const status = (row === rows - 1) ? 'valid' : 'open';
-    return { id: `${col},${row}`, status };
+    return { id: makeId(col, row), status };
   });
 
   const [board, setBoard] = useState(initialBoard);
   const [playerIndex, setPlayerIndex] = useState(0);
+  const [gameResult, setGameResult] = useState(activeGame);
   const [motif, setMotif] = useState('default');
-
-  const togglePlayer = () => playerIndex ? setPlayerIndex(0) : setPlayerIndex(1);
-
-  const getAboveId = id => {
-    const rowAbove = +id[2] - 1;
-    return id[0] + id[1] + rowAbove;
-  };
 
   const playSquare = id => {
     setBoard(board.map(square => {
       if(square.id === id) {
-        return { ...square, status: colors[playerIndex] };
+        return { ...square, status: getPlayerColor() };
       } 
       if(square.id === getAboveId(id)) {
         return { ...square, status: 'valid' };
@@ -43,14 +51,25 @@ const ConnectFour = ({ playerNames }) => {
   const handlePlay = ({ status, id }) => {
     if(status === 'valid') {
       playSquare(id);
+      game.play(getColumn(id));
+      const { gameOver, winner } = game.gameStatus();
+      if(gameOver && winner) {
+        return setGameResult(`${getPlayerName()} (${getPlayerColor()}) wins!`);
+      }
+      if(gameOver) {
+        return setGameResult('It is a draw.');
+      }
       togglePlayer();
     }
   };
 
   return (
     <>
-      <p>{`${playerNames[playerIndex]}'s turn (${colors[playerIndex]})`}</p>
-      <Board board={board} handleClick={handlePlay} motif={motif} />
+      {gameResult ?
+        (<p>{`${gameResult} --- GAME OVER. `}</p>) :
+        (<p>{`${getPlayerName()}'s turn (${getPlayerColor()})`}</p>)
+      }
+      <Board board={board} active={gameIsActive()} handleClick={handlePlay} motif={motif} />
       <p>Checker style: 
         <label onClick={() => setMotif('default')}>
           <input type='radio' id='default' name='motif' value='default' defaultChecked />
