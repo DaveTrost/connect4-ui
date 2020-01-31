ef888b13aefd3b0589e8722e5a0b11526bdfabcd
Dave Trost <davetrost@gmail.com>
2020-01-30 16:03:40 -0800

    refactor part 1 - game status is part of state

--- src/components/connect-four/ConnectFour.js
+++ src/components/connect-four/ConnectFour.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Board from '../board/Board';
 import Connect4 from 'connect4-ai';
@@ -9,67 +9,81 @@ const ConnectFour = ({ playerNames }) => {
   const colors = ['red', 'black'];
   const columns = 7;
   const rows = 6;
-  const activeGame = '';
   if(!playerNames) {
     playerNames = ['player 1', 'player 2'];
   }
 
-  const togglePlayer = () => playerIndex ? setPlayerIndex(0) : setPlayerIndex(1);
   const getPlayerName = () => playerNames[playerIndex];
   const getPlayerColor = () => colors[playerIndex];
-  const gameIsActive = () => gameResult === activeGame;
   
+  const makeDisplay = (name, color) => `${name}'s turn (${color})`;
   const makeId = (col, row) => `${col},${row}`;
   const getRow = id => +id[2];
   const getColumn = id => +id[0];
   const getAboveId = id => makeId(getColumn(id), getRow(id) - 1);
 
+  const initialStatus = makeDisplay(playerNames[0], colors[0]);
   const initialBoard = Array.from({ length: rows * columns }, (_, i) => {
     const row = Math.floor(i / columns);
     const col = i % columns;
     const status = (row === rows - 1) ? 'valid' : 'open';
     return { id: makeId(col, row), status };
   });
-
-  const [board, setBoard] = useState(initialBoard);
+  
+  const [status, setStatus] = useState(initialStatus);
   const [playerIndex, setPlayerIndex] = useState(0);
-  const [gameResult, setGameResult] = useState(activeGame);
+  const [gameOver, endGame] = useState(false);
+  const [board, setBoard] = useState(initialBoard);
   const [motif, setMotif] = useState('default');
+  
+  useEffect(() => {
+    if(gameOver) {
+      setBoard(board.map(square => ({ 
+        ...square, 
+        status: (square.status === 'valid') ? 'open' : square.status
+      })));
+    }
+  }, [gameOver]);
 
-  const playSquare = id => {
+  const updateBoard = ({ playedId }) => {
     setBoard(board.map(square => {
-      if(square.id === id) {
+      if(square.id === playedId) {
         return { ...square, status: getPlayerColor() };
       } 
-      if(square.id === getAboveId(id)) {
+      if(square.id === getAboveId(playedId)) {
         return { ...square, status: 'valid' };
       }
       return square;
     }));
   };
 
-  const handlePlay = ({ status, id }) => {
-    if(status === 'valid') {
-      playSquare(id);
-      game.play(getColumn(id));
-      const { gameOver, winner } = game.gameStatus();
-      if(gameOver && winner) {
-        return setGameResult(`${getPlayerName()} (${getPlayerColor()}) wins!`);
-      }
-      if(gameOver) {
-        return setGameResult('It is a draw.');
-      }
-      togglePlayer();
+  const updateStatus = ({ gameOver, winner }) => {
+    if(gameOver) {
+      endGame(true);
+      setStatus(winner ?
+        `${getPlayerName()} (${getPlayerColor()}) wins!` :
+        'Game Over. It is a draw.'
+      );
     }
+    else {
+      const nextPlayerIndex = playerIndex ? 0 : 1;
+      setPlayerIndex(nextPlayerIndex);
+      setStatus(makeDisplay(playerNames[nextPlayerIndex], colors[nextPlayerIndex]));
+    }
+  };
+
+  const handlePlay = ({ id }) => {
+    if(!game.canPlay(getColumn(id)) || gameOver) return;
+    
+    game.play(getColumn(id));
+    updateStatus(game.gameStatus());
+    updateBoard({ playedId: id });
   };
 
   return (
     <>
-      {gameResult ?
-        (<p>{`${gameResult} --- GAME OVER. `}</p>) :
-        (<p>{`${getPlayerName()}'s turn (${getPlayerColor()})`}</p>)
-      }
-      <Board board={board} active={gameIsActive()} handleClick={handlePlay} motif={motif} />
+      <p>{status}</p>
+      <Board board={board} handleClick={handlePlay} motif={motif} />
       <p>Checker style: 
         <label onClick={() => setMotif('default')}>
           <input type='radio' id='default' name='motif' value='default' defaultChecked />
