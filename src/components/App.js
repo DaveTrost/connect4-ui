9aaf876095cb06afb240c87add791b4daf8db927
Dave Trost <davetrost@gmail.com>
2020-02-02 23:56:25 -0800

    options specified at App level

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ConnectFour from './connect-four/ConnectFour';
 
 export default function App() {
+  const options = {
+    playerNames: ['Player 1', 'Player 2'],
+    humanVsHuman: true,
+    computerFirst: false,
+    aiDifficulty: 'easy',
+    userMotif: 'default',
+  };
   return (
     <>
-      {/* <ConnectFour playerNames={['Human', 'Computer']}/> */}
-      <ConnectFour />
+      <ConnectFour options={options} />
     </>
   );
 }
