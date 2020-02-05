552fc9dd01f21543507b1ec89024af680ffb03ef
Dave Trost <davetrost@gmail.com>
2020-02-05 14:30:56 -0800

    use new images in the checker component

--- src/components/board/Checker.js
+++ src/components/board/Checker.js
@@ -7,17 +7,17 @@ import dog from '../../assets/dog.png';
 import cat from '../../assets/cat.jpg';
 import beer from '../../assets/beer.png';
 import wine from '../../assets/wine.png';
+import blackUnicorn from '../../assets/unicorn-black.jpg';
+import whiteUnicorn from '../../assets/unicorn-white.jpg';
 import mermaid from '../../assets/mermaid-red.jpg';
-import unicorn from '../../assets/unicorn-black-rainbow.jpg';
-import oceanBlue from '../../assets/ocean-blue.jpg';
-import oceanPink from '../../assets/ocean-pink.jpg';
+import narwhal from '../../assets/narwhal.jpg';
 
 const getPieceStyle = motif => {
   const motifReference = {
     checkers: false,
     pets: false,
     drinks: false,
-    fantasy: false,
+    unicorns: false,
     ocean: false,
   };
   if(motifReference[motif] === false) {
@@ -31,15 +31,15 @@ const getPieceStyle = motif => {
 const makeStyle = src => ({ backgroundImage: `url(${src})` });
 
 const Checker = ({ status, motif, isSolution }) => {
-  const { checkers, pets, drinks, fantasy, ocean } = getPieceStyle(motif);
+  const { checkers, pets, drinks, unicorns, ocean } = getPieceStyle(motif);
 
   const redPiece = (
     <>
       {checkers && <div style={makeStyle(redChecker)} className={`${styles.redChecker} ${isSolution ? styles.isSolution : ''}`}></div>}
       {pets && <div style={makeStyle(dog)} className={`${styles.dog} ${isSolution ? styles.isSolution : ''}`}></div>}
       {drinks && <div style={makeStyle(beer)} className={`${styles.beer} ${isSolution ? styles.isSolution : ''}`}></div>}
-      {fantasy && <div style={makeStyle(mermaid)} className={`${styles.mermaid} ${isSolution ? styles.isSolution : ''}`}></div>}
-      {ocean && <div style={makeStyle(oceanPink)} className={`${styles.shark} ${isSolution ? styles.isSolution : ''}`}></div>}
+      {unicorns && <div style={makeStyle(whiteUnicorn)} className={`${styles.whiteUnicorn} ${isSolution ? styles.isSolution : ''}`}></div>}
+      {ocean && <div style={makeStyle(mermaid)} className={`${styles.mermaid} ${isSolution ? styles.isSolution : ''}`}></div>}
     </>
   );
   const blackPiece = (
@@ -47,8 +47,8 @@ const Checker = ({ status, motif, isSolution }) => {
       {checkers && <div style={makeStyle(blackChecker)} className={`${styles.blackChecker} ${isSolution ? styles.isSolution : ''}`}></div>}
       {pets && <div style={makeStyle(cat)} className={`${styles.cat} ${isSolution ? styles.isSolution : ''}`}></div>}
       {drinks && <div style={makeStyle(wine)} className={`${styles.wine} ${isSolution ? styles.isSolution : ''}`}></div>}
-      {fantasy && <div style={makeStyle(unicorn)} className={`${styles.unicorn} ${isSolution ? styles.isSolution : ''}`}></div>}
-      {ocean && <div style={makeStyle(oceanBlue)} className={`${styles.narwhal} ${isSolution ? styles.isSolution : ''}`}></div>}
+      {unicorns && <div style={makeStyle(blackUnicorn)} className={`${styles.blackUnicorn} ${isSolution ? styles.isSolution : ''}`}></div>}
+      {ocean && <div style={makeStyle(narwhal)} className={`${styles.narwhal} ${isSolution ? styles.isSolution : ''}`}></div>}
     </>
   );
   const openSpace = (
@@ -58,7 +58,7 @@ const Checker = ({ status, motif, isSolution }) => {
   return (
     <section className={styles.Checker}>
       {status === 'red' && redPiece}
-      {status === 'pink' && redPiece}
+      {status === 'white' && redPiece}
       {status === 'black' && blackPiece}
       {status === 'blue' && blackPiece}
       {status === 'valid' && openSpace}
@@ -68,7 +68,7 @@ const Checker = ({ status, motif, isSolution }) => {
 };
 
 Checker.propTypes = {
-  status: PropTypes.oneOf(['red', 'black', 'pink', 'blue', 'open', 'valid']).isRequired,
+  status: PropTypes.oneOf(['red', 'black', 'white', 'blue', 'open', 'valid']).isRequired,
   isSolution: PropTypes.bool,
   motif: PropTypes.string,
 };
